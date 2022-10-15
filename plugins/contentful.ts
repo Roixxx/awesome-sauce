848200487f38ce78131408fad09afa023ccf9fcb
Max <vosroix@gmail.com>
2022-10-15 20:26:57 +0500

    Update contentful.ts

--- plugins/contentful.ts
+++ plugins/contentful.ts
@@ -6,8 +6,8 @@ export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
   const createClientFunc = process.env.NODE_ENV === 'development' ? createClient : contentful.createClient;
   const client = createClientFunc({
-    space: config.public.contentfulSpace,
-    accessToken: config.public.contentfulPublicAccessToken,
+    space: process.env.NUXT_CONTENTFUL_SPACE,
+    accessToken: process.env.NUXT_CONTENTFUL_PUBLIC_ACCESS_TOKEN,
   });
   return {
     provide: {
